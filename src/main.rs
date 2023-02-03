0792eea194af3308484caf2f3fdb1a3ad28c6b97
jerry <calljerryli@outlook.com>
2023-02-03 22:39:26 +0800

    set PORT in .env

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,11 @@ async fn main() -> Result<(), tp::error::AppError> {
     let app = axum::Router::new().route("/", axum::routing::get(tp::controller::index::index));
 
     // run app
-    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
+    let port = std::env::var("PORT")
+        .expect("PORT must be set")
+        .parse::<u16>()
+        .expect("PORT must be unsigned number");
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
